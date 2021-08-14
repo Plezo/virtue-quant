2deb408764ce48fa8ef42aa2092e703f07563dab
Patryk Lezon <Plezo@protonmail.com>
2021-08-14 00:47:56 -0400

    Implemented functions for model training

--- packages/models.py
+++ packages/models.py
@@ -1,11 +1,10 @@
 """
 TODO
 
-------------------------------------------------------------------------------------------------------------
-1. In prepare_for_model add functionality to the end_date and num_days params.
-2. Implement the add_TA function (Eventually scale it so you can pass in specific indicators as param)
-3. Fix generate_model function to implement the two helper functions above it
-------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------
+1. Implement Predictions function
+2. Consider switching to a log regression giving probability of profit 0-1
+--------------------------------------------------------------------------------------------------------------
 
 
 """
@@ -19,23 +18,24 @@ from itertools import product
 import pandas as pd
 import pickle
 from sklearn.linear_model import LinearRegression
+from packages import ta
+
+def add_TA(df):
+    ta.add_ATR(df, inplace=True)
+    ta.add_OBV(df, inplace=True)
+    return df
 
 def prepare_for_model(df, end_date, num_days):
-    df = df[['Close', 'QuoteVolume', 'Open']]
+    end_date = pd.to_datetime(end_date)
+
+    df.drop(columns={'High', 'Low', 'Volume'}, inplace=True)
     df['Open'] = df['Open'].shift(-1)
-    df.rename(columns={'Open': 'Target', 'QuoteVolume': 'Volume'})
+    df.rename(columns={'Open': 'Target', 'QuoteVolume': 'Volume'}, inplace=True)
+    df = df[df.index.year <= end_date.year].tail(num_days)
 
-
-def add_TA(df, inplace=True):
-    pass
-
+    return df
 
 def generate_model(asset_name, df, end_date, num_days, force_overwrite=False):
-
-    # Consider either leaving this, or just prepping for model beforehand
-    prepped_df = prepare_for_model(df, end_date, num_days)
-
-
     parent_path = os.path.join('models', asset_name + '_' + end_date + "-linear-" + str(num_days))
 
     if os.path.isfile(os.path.join(parent_path, 'completed.txt')):
@@ -46,25 +46,49 @@ def generate_model(asset_name, df, end_date, num_days, force_overwrite=False):
         shutil.rmtree(parent_path)
 
     os.makedirs(parent_path, exist_ok=force_overwrite)
-
-    try:
-        with mp.Pool(processes=mp.cpu_count()) as pool:
-            list(tqdm(pool.imap(generate_model_helper, product([asset_name], [prepped_df], [end_date], [num_days], [parent_path])), total=len(prepped_df)))
-
-        with open(os.path.join(parent_path, 'completed.txt'), 'w') as file:
-            file.write("This set of models is completely trained.")
-
-    except Exception as e:
-        print("Error:", e)
-        print("Deleting new directory at:", parent_path)
-        print("An error occured.")
-        shutil.rmtree(parent_path)
-        exit()
-
-def generate_model_helper(p):
-    asset_name, prepped_df, end_date, num_days, parent_path = p
-    path_to_save = os.path.join(parent_path, asset_name)
-
+    
     model = LinearRegression()
-    model.fit(X, y)
-    pickle.dump(model, open(path_to_save + ".pkl", 'wb'))
+    tqdm(model.fit(df.loc[:, df.columns != 'Target'], df['Target']), total=len(df))
+    pickle.dump(model, open(os.path.join(parent_path, asset_name) + ".pkl", 'wb'))
+
+    # try:
+    #     with mp.Pool(processes=mp.cpu_count()) as pool:
+    #         list(tqdm(pool.imap(generate_model_helper, product([asset_name], [df], [end_date], [num_days], [parent_path])), total=len(df)))
+
+    #     with open(os.path.join(parent_path, 'completed.txt'), 'w') as file:
+    #         file.write("This set of models is completely trained.")
+
+    # except Exception as e:
+    #     print("Error:", e)
+    #     print("Deleting new directory at:", parent_path)
+    #     print("An error occured.")
+    #     shutil.rmtree(parent_path)
+    #     exit()
+
+# def generate_model_helper(p):
+#     asset_name, df, end_date, num_days, parent_path = p
+#     path_to_save = os.path.join(parent_path, asset_name)
+
+#     model = LinearRegression()
+#     model.fit(df.loc[:, df.columns != 'Target'], df['Target'])
+#     pickle.dump(model, open(path_to_save + ".pkl", 'wb'))
+
+def load_model(asset_name, end_date, num_days):
+    parent_path = os.path.join('models', asset_name + '_' + end_date + "-linear-" + str(num_days))
+    return pickle.load(open(os.path.join(parent_path, asset_name) + ".pkl", 'rb'))
+
+def get_predictions(lrmodel, asset_name, end_date, num_days, message=False):
+    parent_path = os.path.join('models', asset_name + '_' + end_date + "-linear-" + str(num_days))
+
+    if os.path.isfile(os.path.join(parent_path, 'predictions.pkl')):
+        if message:
+            print("Loading saved predictions...")
+        return pd.read_pickle(os.path.join(parent_path, 'predictions.pkl'))
+    else:
+        print("Predictions not detected. Calculating and saving...")
+        with mp.Pool(processes=mp.cpu_count()) as pool:
+            results = list(tqdm(pool.imap(prediction_helper, product([common_features], investible_universe, [lrmodels], [end_date], [num_days], [pycaret])), total=len(investible_universe)))
+
+        predictions = pd.concat(results, axis=0)
+        predictions.to_pickle(os.path.join(parent_path, 'predictions.pkl'))
+        return predictions
